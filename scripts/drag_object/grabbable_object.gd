12f803e28fa02e2da9c415e4e3cdf1fae3baa413
doda-s <ph005009004@gmail.com>
2025-12-14 11:11:05 -0300

    add signal connection verification to grabbable objects

--- scripts/drag_object/grabbable_object.gd
+++ scripts/drag_object/grabbable_object.gd
@@ -13,9 +13,11 @@ var chosen = false
 func _ready():
 	add_to_group("grabbable_objects_group")
 	if has_signal("mouse_entered"):
-		connect("mouse_entered", Callable(self, "_on_mouse_entered"))
+		if not is_connected("mouse_entered", Callable(self, "_on_mouse_entered")):
+			connect("mouse_entered", Callable(self, "_on_mouse_entered"))
 	if has_signal("mouse_exited"):
-		connect("mouse_exited", Callable(self, "_on_mouse_exited"))
+		if not is_connected("mouse_entered", Callable(self, "_on_mouse_entered")):
+			connect("mouse_exited", Callable(self, "_on_mouse_exited"))
 
 func _input(event):
 	if event is InputEventMouseButton:
